194eb5bc7908316376ae928809f036eca720268a
Max <maxswaine@gmail.com>
2023-08-05 16:35:16 +0100

    created basic layout and framework for game. issue with the input button being valid but when it's valid it removes all of the objects created

---
+++ src/data/words.ts
@@ -0,0 +1,54 @@
+import { Word } from "./types";
+
+export const originalWords: Word[] = [
+  {
+    id: 1,
+    word: "snobbish",
+    isWordTyped: false,
+  },
+  {
+    id: 2,
+    word: "exotic",
+    isWordTyped: false,
+  },
+  {
+    id: 3,
+    word: "tenuous",
+    isWordTyped: false,
+  },
+  {
+    id: 4,
+    word: "dull",
+    isWordTyped: false,
+  },
+  {
+    id: 5,
+    word: "spot",
+    isWordTyped: false,
+  },
+  {
+    id: 6,
+    word: "apparel",
+    isWordTyped: false,
+  },
+  {
+    id: 7,
+    word: "plucky",
+    isWordTyped: false,
+  },
+  {
+    id: 8,
+    word: "magic",
+    isWordTyped: false,
+  },
+  {
+    id: 9,
+    word: "amused",
+    isWordTyped: false,
+  },
+  {
+    id: 10,
+    word: "ablaze",
+    isWordTyped: false,
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,77 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Word } from "./data/types";
+import { originalWords } from "./data/words";
+import "./style.scss";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const gameContainer = document.querySelector(".game-square__grid");
+const input = document.querySelector<HTMLInputElement>(".gameinput");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+if (!gameContainer) {
+  throw new Error("Issue with game container");
+}
+
+// Keyboard invader
+
+// Need objects that have properties including the word and whether it is shot or not
+
+// INPUT BAR
+// Input bar at the bottom that allows the user to type the words.
+// First key has to be pressed before the game starts
+// Spacebar has to be pressed before the word is submitted
+// Pressing spacebar clears the input after the word has been submitted
+//
+
+// WORDS TO TYPE
+// The words that need to be typed are in an original array of objects
+// Word Number: number, word: string, hasBeenTyped: Boolean
+// The words on screen are copied from that array and then used separately so that they can be removed, whilst maintaining the original data
+// The copied array is then being mapped to a new array that minuses the words that are typed in the input
+// Potentially use a speed-typing API to get the words that need to be typed so its not the same array every time
+
+const gameArray: Word[] = [...originalWords];
+
+const createWordElement = (word: string) => {
+  const container = document.createElement("div");
+  container.classList.add("word__container");
+
+  const content = document.createElement("div");
+  content.classList.add("word__content");
+
+  const image = document.createElement("img");
+  image.classList.add("word__image");
+  image.src = "/src/images/space-invaders-svgrepo-com.svg";
+
+  const text = document.createElement("p");
+  text.classList.add("word__text");
+  text.textContent = word;
+
+  content.appendChild(image);
+  content.appendChild(text);
+  container.appendChild(content);
+
+  return container;
+};
+
+const printWords = (wordsToType: Word[]) => {
+  const fragment = document.createDocumentFragment();
+  wordsToType.forEach((word: Word) => {
+    const wordElement = createWordElement(word.word);
+    fragment.appendChild(wordElement);
+  });
+  gameContainer.appendChild(fragment);
+};
+
+printWords(gameArray);
+
+// MOVING OBJECTS
+// Effectively a card that exists or not depending on whether the word has been written.
+// Invisible grid that each object moves through. When it gets to the first/last element in the column, it moves down a row
+// if the last object is in the last column and last row then game over
+// IF STATEMENT. IF ROW NUMBER = EVEN HAVE ONE UFO AND IF IT IS ODD HAVE ANOTHER
+
+// Function ideas:
+// function for filtering whether a word has been written:
+// take the string from the input
+// see if it matches any word in the object array
+// remove html element from the word card
+
+// Reset button at the bottom
