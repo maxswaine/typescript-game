741e75bb4d991023d07a3802101b73e3c7bc58f1
Max <maxswaine@gmail.com>
2023-08-07 17:12:39 +0100

    changed a lot of the concept of the game. Currently working on a speedtyping game and have written basic functions that aren't fully functional on the webpage yet. Saving progress

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,5 +1,16 @@
-export type Word = {
-    id: number;
-    word: string;
-    isWordTyped: boolean; 
-}
+export type RandomWord = {
+    readonly word: string
+};
+
+export type QuoteResponse = {
+    readonly _id: string;
+    readonly content: string;
+    readonly author: string;
+    readonly tags: string[];
+    readonly authorSlug: string;
+    readonly length: number;
+    readonly dateAdded: Date;
+    readonly dateModified: Date;
+  };
+  
+  export type API
--- src/main.ts
+++ src/main.ts
@@ -1,77 +1,59 @@
-import { Word } from "./data/types";
-import { originalWords } from "./data/words";
-import "./style.scss";
+import "./styles/main.scss";
+import { RandomWord, QuoteResponse } from "./data/types";
 
-const gameContainer = document.querySelector(".game-square__grid");
-const input = document.querySelector<HTMLInputElement>(".gameinput");
+const gameInput = document.querySelector(".game-input") as HTMLInputElement;
+const quoteContainer = document.querySelector(
+  ".game-square__quote"
+) as HTMLElement;
+const quoteSpanArray = document.querySelectorAll("span");
 
-if (!gameContainer) {
-  throw new Error("Issue with game container");
+if (!gameInput) {
+  throw new Error("Issue with input box");
 }
 
-// Keyboard invader
+if (!quoteContainer) {
+  throw new Error("Issue with input area");
+}
 
-// Need objects that have properties including the word and whether it is shot or not
-
-// INPUT BAR
-// Input bar at the bottom that allows the user to type the words.
-// First key has to be pressed before the game starts
-// Spacebar has to be pressed before the word is submitted
-// Pressing spacebar clears the input after the word has been submitted
-//
-
-// WORDS TO TYPE
-// The words that need to be typed are in an original array of objects
-// Word Number: number, word: string, hasBeenTyped: Boolean
-// The words on screen are copied from that array and then used separately so that they can be removed, whilst maintaining the original data
-// The copied array is then being mapped to a new array that minuses the words that are typed in the input
-// Potentially use a speed-typing API to get the words that need to be typed so its not the same array every time
-
-const gameArray: Word[] = [...originalWords];
-
-const createWordElement = (word: string) => {
-  const container = document.createElement("div");
-  container.classList.add("word__container");
-
-  const content = document.createElement("div");
-  content.classList.add("word__content");
-
-  const image = document.createElement("img");
-  image.classList.add("word__image");
-  image.src = "/src/images/space-invaders-svgrepo-com.svg";
-
-  const text = document.createElement("p");
-  text.classList.add("word__text");
-  text.textContent = word;
-
-  content.appendChild(image);
-  content.appendChild(text);
-  container.appendChild(content);
-
-  return container;
+const getRandomWord = async () => {
+  const response = await fetch(
+    `https://random-word-api.herokuapp.com/word?number=${i}`
+  );
+  const word: RandomWord = await response.json();
+  quoteContainer.innerText = word.word;
+  gameInput.value = "";
 };
 
-const printWords = (wordsToType: Word[]) => {
-  const fragment = document.createDocumentFragment();
-  wordsToType.forEach((word: Word) => {
-    const wordElement = createWordElement(word.word);
-    fragment.appendChild(wordElement);
+const getRandomQuote = async () => {
+  const response = await fetch("https://api.quotable.io/random");
+  const quoteResponse: QuoteResponse = await response.json();
+  quoteContainer.innerText = "";
+  quoteResponse.content.split("").forEach((character) => {
+    quoteContainer.innerHTML += `<span>${character}</span>`;
   });
-  gameContainer.appendChild(fragment);
+  gameInput.value = "";
 };
 
-printWords(gameArray);
+const checkMatchingValues = () => {
+  const inputValue = gameInput.value.split("");
+  quoteSpanArray.forEach((character, i) => {
+    const char = inputValue[i];
+    if (char == null) {
+      character.classList.remove("correct");
+      character.classList.remove("incorrect");
+    } else if (char === character.innerText) {
+      character.classList.add("correct");
+      character.classList.remove("incorrect");
+    } else {
+      character.classList.remove("correct");
+      character.classList.add("incorrect");
+    }
+  });
+};
 
-// MOVING OBJECTS
-// Effectively a card that exists or not depending on whether the word has been written.
-// Invisible grid that each object moves through. When it gets to the first/last element in the column, it moves down a row
-// if the last object is in the last column and last row then game over
-// IF STATEMENT. IF ROW NUMBER = EVEN HAVE ONE UFO AND IF IT IS ODD HAVE ANOTHER
+getRandomQuote();
 
-// Function ideas:
-// function for filtering whether a word has been written:
-// take the string from the input
-// see if it matches any word in the object array
-// remove html element from the word card
+// var Timer = require("timer-machine");
+// var myTimer = new Timer();
 
-// Reset button at the bottom
+gameInput.addEventListener("input", checkMatchingValues);
