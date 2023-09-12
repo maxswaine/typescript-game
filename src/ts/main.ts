614959b6323303016ba6e69677050e1be1b194a5
maxswaine <108192224+maxswaine@users.noreply.github.com>
2023-09-12 20:39:15 +0100

    apply stash

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -56,7 +56,7 @@ if (
 export const getRandomWord = async () => {
   try {
     const response = await fetch(
-      `https://random-word-api.herokuapp.com/word?number=${level}`
+      `https://api.quotable.io/random`
     );
     if (!response.ok) {
       throw new Error("Failed to fetch random words");
